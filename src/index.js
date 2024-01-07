ff0b6f626c6eb716ce7324557166649c7bc850f1
richidmnt <richi1998-@hotmail.com>
2024-01-07 18:29:52 -0500

    actualizar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
-import express from "express"
-import  indexRouters from './routers/index.routers.js'
-import empleadosRouters from './routers/empleados.routers.js'
-const app=express()
-app.use(express.json())
-app.use(indexRouters)
-app.use(empleadosRouters)
+import app from './app.js'
+import {PORT} from './config.js'
 
 
 
-app.listen(3000)
+app.listen(PORT)
+console.log('server',PORT)
--- src/routers/index.routers.js
+++ src/routers/index.routers.js
@@ -1,5 +1,4 @@
 import {Router} from 'express'
-import {pool} from '../db.js'
 import { ping } from '../controllers/index.controllers.js';
 const router = Router()
 router.get('/ping',ping);
